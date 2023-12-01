de1849f6c7788919c52696534a16c3806dae9fe0
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2023-11-30 23:30:53 -0500

    starting lvl is 1-1

--- Assets/Game/6 - Combat/Scripts/2 - State/GameState.cs
+++ Assets/Game/6 - Combat/Scripts/2 - State/GameState.cs
@@ -1,7 +1,7 @@
 public class GameState
 {
-    public int StageNumber = 4;
-    public int WaveNumber = 4;
+    public int StageNumber = 1;
+    public int WaveNumber = 1;
     public int ScalesOwned = 0;
     public int LivesLeft = 3;
 }
